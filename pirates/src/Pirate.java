081d6a8e9c596889cd7ecbfc33cf6b55319c6b4a
zsiguli <markovichzsigmond@gmail.com>
2017-04-10 16:46:22 +0200

    refactor(pirate): separate pirate class

---
+++ pirates/src/Pirate.java
@@ -0,0 +1,12 @@
+public class Pirate {
+
+  String name;
+  boolean hasWoodenLeg;
+  int gold;
+
+  public Pirate(String name, boolean hasWoodenLeg, int gold) {
+    this.name = name;
+    this.hasWoodenLeg = hasWoodenLeg;
+    this.gold = gold;
+  }
+}
