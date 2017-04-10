a3820bc6e4c046d9a4d668ee129ec09d073106d1
zsiguli <markovichzsigmond@gmail.com>
2017-04-10 15:26:45 +0200

    feat(cache-data): get data from given txt file as a list

--- countas/src/CountAs.java
+++ countas/src/CountAs.java
@@ -1,8 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CountAs {
   public static void main(String[] args) {
+    List<String> rawDataFromFile = getListFromFile("afile.txt");
+  }
 
+  public static List<String> getListFromFile(String fileName) {
+    Path path = Paths.get(fileName);
+    List<String> listFromFile;
+    try {
+      listFromFile = Files.readAllLines(path);
+    } catch (IOException e) {
+      listFromFile = new ArrayList<>();
+      System.out.println("Something wrong happened, during the file read. Check afile.txt");
+    }
+    return listFromFile;
   }
 }
