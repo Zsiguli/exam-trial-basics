a7469d81d7b9c7ba777fbba19510f60b00699ef8
zsiguli <markovichzsigmond@gmail.com>
2017-04-10 15:53:35 +0200

    feat(letter-count): count the letter in given file

--- countas/src/CountAs.java
+++ countas/src/CountAs.java
@@ -3,11 +3,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CountAs {
   public static void main(String[] args) {
-    List<String> rawDataFromFile = getListFromFile("afile.txt");
+    List<String> rawDataFromFile = getListFromFile("file.txt");
+    Map<Character, Integer> letterOccurrences = new HashMap<>();
+    for (String line : rawDataFromFile) {
+      for (int i = 0; i < line.length(); ++i) {
+        char currentChar = line.charAt(i);
+        if (letterOccurrences.get(currentChar) == null) {
+          letterOccurrences.put(currentChar, 1);
+        } else {
+          int currentValue = letterOccurrences.get(currentChar);
+          letterOccurrences.put(currentChar, ++currentValue);
+        }
+      }
+    }
+    System.out.println(letterOccurrences.get('a'));
   }
 
   public static List<String> getListFromFile(String fileName) {
@@ -17,7 +32,6 @@ public class CountAs {
       listFromFile = Files.readAllLines(path);
     } catch (IOException e) {
       listFromFile = new ArrayList<>();
-      System.out.println("Something wrong happened, during the file read. Check afile.txt");
     }
     return listFromFile;
   }
