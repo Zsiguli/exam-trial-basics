6c859e7c13c8ef6abbea3738241f6cd7363ec5be
zsiguli <markovichzsigmond@gmail.com>
2017-04-10 15:01:08 +0200

    feat(test-everySecond): test ready for method Seconds.returnEverySedonsFromGivenList()

---
+++ seconds/tests/SecondsTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SecondsTest {
+
+  @Test
+  public void testSeconds() {
+    List<Integer> originalList = new ArrayList<>();
+    originalList.add(2);
+    originalList.add(4);
+    originalList.add(8);
+    originalList.add(19);
+    List<Integer> testList = new ArrayList<>();
+    testList.add(4);
+    testList.add(8);
+    assertEquals(Seconds.returnEverySecondFromGivenList(originalList), testList);
+  }
+}
