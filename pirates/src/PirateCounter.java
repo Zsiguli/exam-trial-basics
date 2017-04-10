9c4513205670c0cf62cf10b25c3280642c2cd38a
zsiguli <markovichzsigmond@gmail.com>
2017-04-10 16:55:56 +0200

    refactor(pirateCounter): share list, rename func

--- pirates/src/PirateCounter.java
+++ pirates/src/PirateCounter.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
 public class PirateCounter {
+  static ArrayList<Pirate> pirates = new ArrayList<>();
 
   public static void main(String... args) {
-    ArrayList<Pirate> pirates = new ArrayList<>();
+
     pirates.add(new Pirate("Olaf", false, 12));
     pirates.add(new Pirate("Uwe", true, 9));
     pirates.add(new Pirate("Jack", true, 16));
     pirates.add(new Pirate("Morgan", false, 17));
     pirates.add(new Pirate("Hook", true, 20));
+    System.out.println(getRichWoodenLegPiratesName(pirates).toString());
   }
 
-  public static List<String> getRichWoodenLegPirates(List<Pirate> pirates) {
-    List<String> richWoodenLegPirates = new ArrayList<>();
+  public static List<String> getRichWoodenLegPiratesName(List<Pirate> pirates) {
+    List<String> richWoodenLegPiratesName = new ArrayList<>();
     for (Pirate pirate : pirates) {
       if (pirate.hasWoodenLeg == true && pirate.gold > 15) {
-        richWoodenLegPirates.add(pirate.name);
+        richWoodenLegPiratesName.add(pirate.name);
       }
     }
-    return richWoodenLegPirates;
+    return richWoodenLegPiratesName;
   }
 }
